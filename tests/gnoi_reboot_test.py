7213545cac8268842a5002e298c5326fc1041ca2
Vasundhara Volam <vvolam@microsoft.com>
2024-09-11 21:09:05 +0000

    Add support for HALT reboot method

--- tests/gnoi_reboot_test.py
+++ tests/gnoi_reboot_test.py
@@ -23,12 +23,14 @@ TEST_INACTIVE_RESPONSE_DATA = "{\"active\": false, \"when\": 0, \"reason\": \"\"
 
 REBOOTMETHOD_UNKNOWN_ENUM = 0
 REBOOTMETHOD_COLD_BOOT_ENUM = 1
+REBOOTMETHOD_HALT_BOOT_ENUM = 3
 REBOOTMETHOD_NSF_ENUM = 5
 
 TEST_TIMESTAMP = 1618942253.831912040
 REPORT_CRITICAL_STATE_FULL_COMMAND = "redis-cli -n 6 HSET COMPONENT_STATE_TABLE|host state ERROR reason \"cold reboot has failed\" essential true timestamp \"2021-04-20 18:10:53\" timestamp-seconds 1618942253 timestamp-nanoseconds 831912040"
 
 VALID_REBOOT_REQUEST_COLD = "{\"method\": 1, \"message\": \"test reboot request reason\"}"
+VALID_REBOOT_REQUEST_HALT = "{\"method\": 3, \"message\": \"test reboot request reason\"}"
 VALID_REBOOT_REQUEST_NSF = "{\"method\": \"NSF\", \"message\": \"test reboot request reason\"}"
 INVALID_REBOOT_REQUEST = "\"method\": 1, \"message\": \"test reboot request reason\""
 
@@ -58,6 +60,18 @@ class TestGnoiReboot(object):
 
     def test_validate_reboot_request_success_cold_boot_string_method(self):
         reboot_request = {"method": "COLD", "reason": "test reboot request reason"}
+        result = self.gnoi_reboot_module.validate_reboot_request(reboot_request)
+        assert result[0] == 0
+        assert result[1] == ""
+
+    def test_validate_reboot_request_success_halt_boot_enum_method(self):
+        reboot_request = {"method": REBOOTMETHOD_HALT_BOOT_ENUM, "reason": "test reboot request reason"}
+        result = self.gnoi_reboot_module.validate_reboot_request(reboot_request)
+        assert result[0] == 0
+        assert result[1] == ""
+
+    def test_validate_reboot_request_success_halt_boot_string_method(self):
+        reboot_request = {"method": "HALT", "reason": "test reboot request reason"}
         result = self.gnoi_reboot_module.validate_reboot_request(reboot_request)
         assert result[0] == 0
         assert result[1] == ""
@@ -124,6 +138,20 @@ class TestGnoiReboot(object):
             assert caplog.records[0].message == msg
             mock_populate_reboot_status_flag.assert_called_once_with()
 
+    def test_execute_reboot_fail_issue_reboot_command_halt_boot(self, caplog):
+        with (
+            mock.patch("gnoi_reboot._run_command") as mock_run_command,
+            mock.patch("gnoi_reboot.GnoiReboot.populate_reboot_status_flag") as mock_populate_reboot_status_flag,
+            caplog.at_level(logging.ERROR),
+        ):
+            mock_run_command.return_value = (1, ["stdout: execute halt reboot"], ["stderror: execute halt reboot"])
+            self.gnoi_reboot_module.execute_reboot(REBOOTMETHOD_HALT_BOOT_ENUM)
+            msg = ("gnoi_reboot: Reboot failed execution with "
+                    "stdout: ['stdout: execute halt reboot'], stderr: "
+                    "['stderror: execute halt reboot']")
+            assert caplog.records[0].message == msg
+            mock_populate_reboot_status_flag.assert_called_once_with()
+
     def test_execute_reboot_fail_issue_reboot_command_nsf(self, caplog):
         with (
             mock.patch("gnoi_reboot._run_command") as mock_run_command,
@@ -150,6 +178,21 @@ class TestGnoiReboot(object):
             mock_thread.assert_called_once_with(
                 target=self.gnoi_reboot_module.execute_reboot,
                 args=(REBOOTMETHOD_COLD_BOOT_ENUM,),
+            )
+            mock_thread.return_value.start.assert_called_once_with()
+
+    def test_issue_reboot_success_halt_boot(self):
+        with (
+            mock.patch("threading.Thread") as mock_thread,
+            mock.patch("gnoi_reboot.GnoiReboot.validate_reboot_request", return_value=(0, "")),
+        ):
+            self.gnoi_reboot_module.populate_reboot_status_flag()
+            result = self.gnoi_reboot_module.issue_reboot([VALID_REBOOT_REQUEST_HALT])
+            assert result[0] == 0
+            assert result[1] == "Successfully issued reboot"
+            mock_thread.assert_called_once_with(
+                target=self.gnoi_reboot_module.execute_reboot,
+                args=(REBOOTMETHOD_HALT_BOOT_ENUM,),
             )
             mock_thread.return_value.start.assert_called_once_with()
 
